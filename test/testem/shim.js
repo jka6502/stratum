13634e03f5a1517086ed140ee34b96f45a98ebed
James Andrews <jka6502@gmail.com>
2014-06-01 02:36:07 +0100

    Version 0.5
    * Change test framework to Mocha
       - Add testem/mocha shim to work with require
       - Move test config to reside under test/
    * Remove 'DOMContentLoaded' replacement
       - Replaced by custom 'DOMScriptsLoaded' event
    * Simplify Script.current() to be universal between browsers
       - Requeue any 'body' scripts during 'DOMContentLoaded' instead
       - Remove old Loader.ready

---
+++ test/testem/shim.js
@@ -0,0 +1,16 @@
+(function() {
+
+	// Prevent testem from running mocha.run on DOMContentLoaded, since we will
+	// only start loading them then - instead, trigger the run from
+	// DOMScriptsLoaded - the custom stratum event.
+	var original = mocha.run;
+	mocha.run = function() {};
+
+
+	window.addEventListener('DOMScriptsLoaded', function() {
+		original.call(mocha);
+	});
+
+
+
+})();
